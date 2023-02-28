4c94c09ef5f4a96c092357f95d0de85da8997974
tamames <arcospgt@gmail.com>
2023-02-28 01:09:42 +0100

    Added main loop

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from classes import Population
+import matplotlib.pyplot as plt
 
 
 def main() -> None:
-    # population_size = 20
-    # population = [Agent(randint(1,15)) for _ in range(population_size)]
-    # population = new_population(population)
-    p = Population(10)
-    p.after_iteration()
-    print(len(p))
+    popu = Population(10)
+    n_generations = 100
+    sizes = [10]
+    for _ in range(n_generations):
+        popu.iteration(30)
+        popu.after_iteration()
+        sizes.append(len(popu))
+    print(sizes)
+
+    plt.plot(sizes)
+    plt.show()
 
 
 if __name__ == "__main__":
