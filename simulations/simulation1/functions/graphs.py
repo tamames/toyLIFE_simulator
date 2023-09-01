e977012343b047c5e848afc549d5c884b6f0376b
tamames <arcospgt@gmail.com>
2023-09-01 16:46:14 +0200

    updated graphs

--- simulations/simulation1/functions/graphs.py
+++ simulations/simulation1/functions/graphs.py
@@ -21,12 +21,11 @@ def get_df(file: str) -> pd.DataFrame:
     Returns:
         pd.DataFrame: the csv as a pandas DataFrame.
     """
-    type_of_file = file[:4]
 
     data_folder = Path("simulations/simulation1/data")
     file_to_open = data_folder / file
 
-    if type_of_file == "food":
+    if file.startswith("food"):
         types = {
             "Max_1s": int,
             "Index": int,
@@ -37,7 +36,7 @@ def get_df(file: str) -> pd.DataFrame:
         }
         df = pd.read_csv(file_to_open, sep=",", header=0, dtype=types, index_col=False)
 
-    elif type_of_file == "geno":
+    elif file.startswith("geno"):
         types = {
             "Max_1s": int,
             "Index": int,
@@ -49,7 +48,7 @@ def get_df(file: str) -> pd.DataFrame:
         }
         df = pd.read_csv(file_to_open, sep=",", header=0, dtype=types, index_col=False)
 
-    elif type_of_file == "popu":
+    elif file.startswith("popu"):
         types = {
             "Generation": int,
             "Average Energy Gain": float,
@@ -77,7 +76,7 @@ def save_graph(fig: mplf.Figure, name: str) -> None:
     fig.savefig(file_to_save)
 
 
-def write_results(file_name: str, description: str) -> None:
+def write_results(file_name: str, description: str, data_source: str = "") -> None:
     """Writes into the Readme to keep track of the figures created.
 
     Args:
@@ -88,10 +87,29 @@ def write_results(file_name: str, description: str) -> None:
     readme_path = Path("simulations/simulation1/graphs/Readme.md")
 
     with open(readme_path, "a") as des_file:
-        des_file.write(file_name + ".png" + " → " + description + "  \n")
+        des_file.write(file_name + ".png" + " &rarr " + description + "  \n")
 
 
-def graph_distribution_of_1(df: pd.DataFrame) -> mplf.Figure:
+def save_and_write(
+    fig: mplf.Figure, name: str, description: str = "", data_source: str = ""
+) -> None:
+    """Handle the call of the save_fig and write_results functions.
+
+    Args:
+        file (mplf.Figure): the figure to save
+        name (str): The name of the file to save
+        description (str): a description to identify the picture.
+    """
+    params_for_descrp = name.split("_")
+    save_graph(fig, name)
+
+    if not description:
+        description = f"Is a {params_for_descrp[0]} plot that represent a {params_for_descrp[1]}. *Data = {data_source}*. **Control={params_for_descrp[-1]}**"
+
+    write_results(name, description, data_source)
+
+
+def bar_only_1(df: pd.DataFrame) -> mplf.Figure:
     df["1s Ratio"] = df["Total 1s"] / df["Total Elements"]
 
     # Create a new figure
@@ -107,7 +125,8 @@ def graph_distribution_of_1(df: pd.DataFrame) -> mplf.Figure:
     return fig
 
 
-def graph_1s_and_0s(df: pd.DataFrame) -> mplf.Figure:
+def stackplot_1_0(df: pd.DataFrame) -> tuple[mplf.Figure, str]:
+    plot_type = "stack"
     df["1s Ratio"] = df["Total 1s"] / df["Total Elements"]
 
     df["0s Ratio"] = 1 - df["1s Ratio"]
@@ -132,16 +151,49 @@ def graph_1s_and_0s(df: pd.DataFrame) -> mplf.Figure:
     ax.legend()
     ax.grid(axis="y", linestyle="--", alpha=0.7)
 
-    return fig
+    return fig, plot_type
+
+
+def get_fig_name(source_name: str, plot_type: str) -> str:
+    """Gives the figure name based on the file name.
+    The figures name have the following structure:
+        plotType_source_control
+    so with the file name we can get what type of plot we are looking at, what is representing
+    (this is just if it comes from a food, geno or popu file) and the control parameter of that simulation,
+    if we want to know the data source we can look at the Readme file.
+
+    Args:
+        source_name (str): the data source
+        plot_type (str): the type of plot
+
+    Returns:
+        str: the name of the figure file
+    """
+    control = source_name.split(".")[0][-2:]
+    return f"{plot_type}_{source_name[:4]}_{control}"
 
 
 def main() -> None:
     file_name = "genotype_info_75.csv"
-    # the last two characters of the file name is the control parameter of the population, that number gives us the way the foodList is distributed.
-    control = file_name.split(".")[0][-2:]
+    # the last two characters of the file name are the control parameter of the population
+    #  that number gives us the way the foodList is distributed.
+
     df = get_df(file_name)
-    fig1 = graph_1s_and_0s(df)
-    save_graph(fig1, "1s_and_0s.png")
+    fig1, plot_type = stackplot_1_0(df)
+
+    fig_name = get_fig_name(file_name, plot_type)
+    save_and_write(fig1, fig_name, data_source=file_name)
+
+
+def do_all(file_name: str) -> None:
+    """This function does everything given a file name
+    The idea is that given a file name the program generates all the valuable plots for that type
+    of data.
+
+    Args:
+        file_name (str): the data source
+    """
+    pass
 
 
 if __name__ == "__main__":
